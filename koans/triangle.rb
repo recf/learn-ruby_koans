7e69521c1bf1ef4e61e553971ee24c92f026b695
Aaron Dorheim-Davis <aaron.bdd@gmail.com>
2015-01-14 17:23:15 -0800

    About Triangles Project 2

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,9 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  raise TriangleError if a <= 0 || b <= 0 || c <= 0
+  raise TriangleError unless a + b > c && a + c > b && b + c > a
+
   if a == b && b == c
     :equilateral
   elsif a == b || b == c || a == c
